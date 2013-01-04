6fb450a8ae99c6143e972ea5736bc7ff86ae4854
PengShulin <Shulin.Peng@fluke.com>
2013-01-04 09:20:15 +0800

    add scripts

---
+++ bin/st-util.d
@@ -0,0 +1,21 @@
+#!/bin/sh
+# run st-util gdbserver looply
+#
+
+# counts reload times
+reload_counter=0
+
+# main loop
+while [ 1 ] ;do
+  echo "Starting st-util daemons"
+  st-util
+  echo "st-util stopped"
+  echo
+  # /bin/sh is aliased to dash, so 'let' is invalid
+  #let reload_counter++
+  reload_counter=$((reload_counter+1)) 
+  echo "Reload for the $reload_counter time"
+  # wait for a while
+  sleep 3
+done
+
