45aec8ac94809fa0bf3a519cd32cb1f5cd0af930
PengShulin <Shulin.Peng@fluke.com>
2013-06-07 16:28:45 +0800

    edit

--- bin/volume.sh
+++ bin/volume.sh
@@ -1,17 +1,28 @@
-#!/bin/bash
+#!/bin/sh
+
+if [ -z '$SOUND_CARD_ID' ]; then
+SOUND_CARD_ID=0
+fi
+
+if [ -z '$SOUND_CARD_CHANNEL' ]; then
+SOUND_CARD_CHANNEL="Master"
+fi
 
 case "$1" in
+    toggle)
+        amixer -q -c $SOUND_CARD_ID set $SOUND_CARD_CHANNEL toggle 
+        ;;
     unmute)
-        amixer -c 0 set Master unmute 
+        amixer -q -c $SOUND_CARD_ID set $SOUND_CARD_CHANNEL unmute 
         ;;
     mute)
-        amixer -c 0 set Master mute 
+        amixer -q -c $SOUND_CARD_ID set $SOUND_CARD_CHANNEL mute 
         ;;
     up)
-        amixer -c 0 set Master 10%+ unmute
+        amixer -q -c $SOUND_CARD_ID set $SOUND_CARD_CHANNEL 5%+ unmute
         ;;
     down)
-        amixer -c 0 set Master 10%- unmute
+        amixer -q -c $SOUND_CARD_ID set $SOUND_CARD_CHANNEL 5%- unmute
         ;;
 esac
 
